b659c8804d7e970068670752234fbfdeb0aaa722
Kaleman60 <kalemancardenas@gmail.com>
2026-05-12 11:53:05 +0200

    modificaciones en el codigo metapython universal

--- metatractor_universal.py
+++ metatractor_universal.py
@@ -11,4 +11,49 @@ def get_image_meta(path):
     meta = {}
     try: 
         with Image.open(path) as imag:
-            exif = img._get
+            exif = img._getexif()
+            if exif:
+                for tag, value in exif.items():
+                    decoded = TAGS.get(tag, tag)
+                    meta[decoded] = str(value)
+
+
+    except : pass
+    return meta
+
+
+def get_pdf_meta(path):
+    meta = {}
+    try: 
+        reader = PdfReader(path)
+        info = reader.metadata
+        for key in info: 
+            meta [key.replace('/','')] = str(info[key])
+
+    except: pass
+    return meta
+
+
+
+def get_office_meta(path):
+    meta = {}
+    try: 
+        if path.endswith('.docx'):
+            doc = Document(path)
+            prop = doc.core_properties
+
+        elif path.endswith('.xlsx'):
+            wb = load_workbook(path)
+            prop = wb.properties
+
+        attrs = ['author', 'created', 'last_modified_by','last_printed','title','version']
+        for attr in attrs:
+            if hasattr(prop, attr):
+                meta[attr] = str(getattr(prop, attr))
+
+    except: pass
+    return meta
+
+
+def main():
+    
